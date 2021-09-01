08afa3357ada947c70602fdb45c423ccbe00f762
yulibo <564885081@qq.com>
2021-09-01 14:20:51 +0800

    修改文件

---
+++ easylibrary/src/main/java/com/example/easylibrary/FirstFun.java
@@ -0,0 +1,7 @@
+package com.example.easylibrary;
+
+public class FirstFun {
+    public boolean statusFunc() {
+        return true;
+    }
+}
--- easylibrary/src/main/java/com/example/easylibrary/easyFrame.java
+++
@@ -1,7 +0,0 @@
-package com.example.easylibrary;
-
-public class EasyFrame {
-    public static int sum (int a, int b) {
-        return a+b;
-    }
-}
